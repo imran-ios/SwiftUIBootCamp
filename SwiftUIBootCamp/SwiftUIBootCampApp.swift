b528c0ac35f6d78e54ce1500a806990d338a0e02
Md Imran Ali test <mdimranali94@gmail.com>
2024-07-28 20:04:39 +0530

    Initial Commit

---
+++ SwiftUIBootCamp/SwiftUIBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootCampApp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Md Imran Ali on 28/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
