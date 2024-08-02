c1e7a93c67eef20628bdeb67aa00f6224a67ac52
Md Imran Ali test <mdimranali94@gmail.com>
2024-08-02 22:10:29 +0530

    Read And implemented SwiftUI Shape

---
+++ SwiftUIBootCamp/ShapeBootCamp.swift
@@ -0,0 +1,27 @@
+//
+//  ShapeBootCamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Md Imran Ali on 02/08/24.
+//
+
+import SwiftUI
+
+struct ShapeBootCamp: View {
+    var body: some View {
+        Ellipse().frame(width: 250, height: 100)
+//        Circle()
+//            .fill(.red)
+//            .foregroundStyle(.green)
+//            .stroke()
+//            .stroke(.red, lineWidth: 2)
+//            .stroke(.green, style: .init(lineWidth: 20, lineCap: .butt, dash: [10,9]))
+//            .trim(from: 0.2, to: 0.7)
+//            .stroke(.yellow, lineWidth: 20)
+        
+    }
+}
+
+#Preview {
+    ShapeBootCamp()
+}
