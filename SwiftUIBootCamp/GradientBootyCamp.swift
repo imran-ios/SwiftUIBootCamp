810d89ec9fc4458b1e886612be36b436e3e3e5b3
Md Imran Ali test <mdimranali94@gmail.com>
2024-09-04 06:50:41 +0530

    [feature ] Learn mesh gradient

--- SwiftUIBootCamp/GradientBootyCamp.swift
+++ SwiftUIBootCamp/GradientBootyCamp.swift
@@ -15,7 +15,18 @@ struct GradientBootyCamp: View {
 //            .fill(LinearGradient(colors: [Color.blue.opacity(0.7), Color.blue], startPoint: .topLeading, endPoint: .bottomTrailing))
 //            .ignoresSafeArea()
 //        RadialGradient(colors: [.yellow, .green], center: .topLeading, startRadius: 250, endRadius: 255)
-        AngularGradient(colors: [.yellow, .green], center: .bottom)
+//        AngularGradient(colors: [.yellow, .green], center: .bottom)
+        
+        MeshGradient(width: 3, height: 3, points: [
+            .init(0, 0), .init(0.5, 0), .init(1, 0),
+            .init(0, 0.5), .init(0.5, 0.5), .init(1, 0.5),
+            .init(0, 1), .init(0.5, 1), .init(1, 1),.init(0.3, 0.7)
+        ], colors: [
+            .red, .purple, .indigo,
+            .orange, .white, .blue,
+            .yellow, .green, .mint
+            ,.black
+        ])
             .ignoresSafeArea()
         
     }
