809a0760ab435f8c1bd145ceaf6eeb543538019d
Md Imran Ali test <mdimranali94@gmail.com>
2024-09-06 06:20:24 +0530

    [Feature] about image and frame

---
+++ SwiftUIBootCamp/ImageBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  imageBootcamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Md Imran Ali on 06/09/24.
+//
+
+import SwiftUI
+
+struct imageBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    imageBootcamp()
+}
