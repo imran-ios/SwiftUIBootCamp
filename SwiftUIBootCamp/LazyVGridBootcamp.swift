e909a319ce8a31a554e3e0e53d139a5e2aca76b5
Md Imran Ali test <mdimranali94@gmail.com>
2024-09-08 11:35:44 +0530

    [feature]  LazyVGrid

--- SwiftUIBootCamp/LazyVGridBootcamp.swift
+++ SwiftUIBootCamp/LazyVGridBootcamp.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct LazyVGridBootcamp: View {
+    let columns = [GridItem(.flexible()),GridItem(.flexible()),GridItem(.flexible())]
+    
+    let rows = [GridItem(.fixed(100))]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView {
+           
+           
+            LazyVGrid(columns: columns) {
+                Section {
+                    ForEach(0..<50) { _ in
+                        
+                        Rectangle()
+                            .frame(height: 150)
+                    }
+                } header: {
+                    
+                    Image("image2")
+                        .resizable()
+                        .frame(height: 250)
+                }
+                
+                
+                
+            }
+        }
     }
 }
 
